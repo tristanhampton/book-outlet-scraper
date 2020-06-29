01f1d04a987a0c21b55f57eddccc5753530b37d7
Tristan Hampton <tristanhampton@icloud.cmo>
2020-06-29 16:48:10 -0600

    adding everything as [object object] from every page, but still not working fully

--- book-outlet-scraper.js
+++ book-outlet-scraper.js
@@ -1,42 +1,68 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-let page = 1
+let pagenumber = 1
 let size = 300
-let url = `https://bookoutlet.ca/Store/Browse?Nc=31&Ns=600-1421&page=${page}&size=${size}&sort=popularity_0`
+let url = `https://bookoutlet.ca/Store/Browse?Nc=31&Ns=600-1421&page=${pagenumber}&size=${size}&sort=popularity_0`
+let i = 0
 
 
 const BookOutletScraper = async() => {
     try {
+        // Open Browser
         const browser = await puppeteer.launch({headless: true});
+        // Open new page
         const page = await browser.newPage();
-
+        // Go to URL
         await page.goto(url)
+        // Wait for selector to appear before continuing
         await page.waitForSelector('a.line-clamp-2')
 
-        const bookList = await page.evaluate(() => {
-            // get data-text from titles
-            let titleNodeList = document.querySelectorAll('a.line-clamp-2');
-            let authorNodeList = document.querySelectorAll('p.author');
-            let formatNodeList = document.querySelectorAll('p.small');
-            let listPriceNodeList = document.querySelectorAll('.price-h6 span:first-of-type span:last-of-type');
-            let salePriceNodeList = document.querySelectorAll('.price-h6 span:last-of-type span');
-            let pagination = document.querySelector('[aria-label="page navigation"]');
-            let catalogueArray = [];
+        // Get the next button to starrt the while loop
+        let nextButton = 0
 
-            for (let i = 0; i < titleNodeList.length; i++) {
-                catalogueArray[i] += {
-                    title: titleNodeList[i].dataset.text,
-                    author: authorNodeList[i].innerText.trim(),
-                    format: formatNodeList[i].innerText.trim(),
-                    listPrice: listPriceNodeList[i].innerText.slice(1),
-                    salePrice: salePriceNodeList[i].innerText.slice(7)
-                }
+
+        
+        let catalogueArray = [];
+
+        try {
+            // While there is still a next button on the pagination, continue scraping
+            while (nextButton != null) {
+                await page.goto(`https://bookoutlet.ca/Store/Browse?Nc=31&Ns=600-1421&page=${pagenumber}&size=${size}&sort=popularity_0`)
+                await page.waitForSelector('a.line-clamp-2')
+    
+                catalogueArray += await page.evaluate((pagenumber) => {
+                    let titleNodeList = document.querySelectorAll('a.line-clamp-2');
+                    let authorNodeList = document.querySelectorAll('p.author');
+                    let formatNodeList = document.querySelectorAll('p.small');
+                    let listPriceNodeList = document.querySelectorAll('.price-h6 span:first-of-type span:last-of-type');
+                    let salePriceNodeList = document.querySelectorAll('.price-h6 span:last-of-type span');
+                    let catalogueArray = [];
+
+                    for (let i = 0; i < titleNodeList.length; i++) {
+                        catalogueArray[i] = {
+                            title: titleNodeList[i].dataset.text,
+                            author: authorNodeList[i].innerText.trim(),
+                            format: formatNodeList[i].innerText.trim(),
+                            listPrice: listPriceNodeList[i].innerText.slice(1),
+                            salePrice: salePriceNodeList[i].innerText.slice(7)
+                        }
+                    }
+                    return catalogueArray
+                })
+                
+                pagenumber++
+                nextButton = await page.evaluate(() => document.querySelector('[aria-label="Next"]')) 
             }
-            return catalogueArray
-        })
+            
+        } catch(err) {
+            console.log(err)
+        }
 
-        fs.writeFile('catalogue.json', JSON.stringify(bookList, null, '\t'), function(err) {
+
+        
+
+        fs.writeFile('catalogue.json', JSON.stringify(catalogueArray, null, '\t'), function(err) {
             if (err) throw err
             console.log('Saved!');
         })
